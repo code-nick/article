37b81dee5338965c25b08fc566c520bb89660e05
code-nick <code.dev.us@gmail.com>
2023-05-08 15:44:50 +0530

    commit

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import profile from '../assets/images/avatar-michelle.jpg'
 import share from '../assets/images/icon-share.svg'
+import facebook from '../assets/images/icon-facebook.svg'
+import twitter from '../assets/images/icon-twitter.svg'
+import pins from '../assets/images/icon-pinterest.svg'
 
 const Hero = () => {
+    const[show,setShow]=useState(false)
+    const share_btn = ()=>{
+        console.log('hello');
+        setShow(!show);
+    }
+    
     return (
         <>
             <main className='d-f '>
@@ -21,12 +30,19 @@ const Hero = () => {
                                     <p>28 jun 2020</p>
                                 </h5>
                             </div>
-                            <div className='share'>
+                            <div className='share' onClick={share_btn}>
                                 <button className='share__btn'>
                                 <img src={share} alt="" className='sharebtn' />
                                 </button>
                             </div>
-
+                        </div>
+                        <div className={` ${show ?'':'hide'} share_svg_btn`}>
+                            <p>share</p>
+                            <img src={facebook} alt="" />
+                            <img src={twitter} alt="" />
+                            <img src={pins} alt="" />
+                            <div className='share_svg_btn__mobile_share'></div>
+                            <div className='share_svg_btn__triangle_down'></div>
                         </div>
                     </div>
                 </article>
